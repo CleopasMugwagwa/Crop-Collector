fe53454b08446e774217fd39593f49e0a5a38141
Cleopas Mugwagwa <cleopasmugwagwa21@gmail.com>
2026-06-05 03:12:18 +0200

    Reduce collector settings clutter

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-﻿// legacy smoke marker: zingsa-crop-collector-v1071-fetch-admin-mbtiles-fix
-const CACHE_NAME = 'crop-collector-v1071-fetch-admin-mbtiles-fix';
+﻿// legacy smoke marker: zingsa-crop-collector-v1072-compact-settings-cleanup
+const CACHE_NAME = 'crop-collector-v1072-compact-settings-cleanup';
 const TRANSPARENT_GIF_BYTES = Uint8Array.from(
   atob('R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw=='),
   (char) => char.charCodeAt(0)
